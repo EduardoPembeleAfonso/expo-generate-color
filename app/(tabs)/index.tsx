5425aae00e3f9fbdb2f22cc78575bfeb611b812e
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2024-11-25 13:38:06 +0100

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,161 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Slider from "@react-native-community/slider";
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 export default function HomeScreen() {
+  const [red, setRed] = useState<number>(73);
+  const [green, setGreen] = useState<number>(255);
+  const [blue, setBlue] = useState<number>(255);
+
+  const backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  const hexColor = `#${((1 << 24) + (red << 16) + (green << 8) + blue)
+    .toString(16)
+    .slice(1)
+    .toUpperCase()}`;
+
+  const handleRedChange = (newText: string) => {
+    const parsedValue = parseInt(newText, 10);
+    setRed(isNaN(parsedValue) ? 0 : parsedValue);
+  };
+  const handleGreenChange = (newText: string) => {
+    const parsedValue = parseInt(newText, 10);
+    setGreen(isNaN(parsedValue) ? 0 : parsedValue);
+  };
+  const handleBlueChange = (newText: string) => {
+    const parsedValue = parseInt(newText, 10);
+    setBlue(isNaN(parsedValue) ? 0 : parsedValue);
+  };
+
+  const copyToClipboard = async (text: string) => {
+    await Clipboard.setStringAsync("hello world");
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <Text
+          style={styles.colorCode}
+          onLongPress={() => copyToClipboard(hexColor)}
+        >
+          {hexColor}
+        </Text>
+
+        <View style={styles.allSlidersContainer}>
+          <View style={styles.sliderContainer}>
+            <View style={styles.sliderContainerHeader}>
+              <Text>R:</Text>
+              <TextInput
+                style={{ height: 40 }}
+                keyboardType="numeric"
+                onChangeText={handleRedChange}
+                defaultValue={red.toString()}
+              />
+            </View>
+            <Slider
+              style={styles.verticalSlider}
+              minimumValue={0}
+              maximumValue={255}
+              step={1}
+              value={red}
+              minimumTrackTintColor="red"
+              onValueChange={setRed}
+            />
+          </View>
+
+          <View style={styles.sliderContainer}>
+            <View style={styles.sliderContainerHeader}>
+              <Text>G:</Text>
+              <TextInput
+                style={{ height: 40 }}
+                keyboardType="numeric"
+                onChangeText={handleGreenChange}
+                defaultValue={green.toString()}
+              />
+            </View>
+            <Slider
+              style={styles.verticalSlider}
+              minimumValue={0}
+              maximumValue={255}
+              step={1}
+              value={green}
+              minimumTrackTintColor="green"
+              onValueChange={setGreen}
+            />
+          </View>
+
+          <View style={styles.sliderContainer}>
+            <View style={styles.sliderContainerHeader}>
+              <Text>B:</Text>
+              <TextInput
+                style={{ height: 40 }}
+                keyboardType="numeric"
+                onChangeText={handleBlueChange}
+                defaultValue={blue.toString()}
+              />
+            </View>
+            <Slider
+              style={styles.verticalSlider}
+              minimumValue={0}
+              maximumValue={255}
+              step={1}
+              value={blue}
+              minimumTrackTintColor="blue"
+              onValueChange={setBlue}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  colorCode: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  allSlidersContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  slidersContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  sliderContainerHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  sliderContainer: {
+    alignItems: "center",
+  },
+  verticalSlider: {
+    width: 200,
+    height: 40,
+    marginTop: 90,
+    transform: [{ rotate: "-90deg" }],
+  },
+  sliderLabel: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
